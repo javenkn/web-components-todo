47868404f1680ceec6cbd96d891a871be754256d
Javen Nakamoto <javenkn7@gmail.com>
2020-02-21 13:59:59 -0800

    Creates TodoApp custom element

---
+++ todo-app.js
@@ -0,0 +1,69 @@
+const template = document.createElement('template');
+template.innerHTML = `
+<style>
+  ul {
+    padding: 0 20px 10px 0;
+    width: 100%;
+    max-height: 450px;
+    overflow-y: auto;
+  }
+
+  input {
+    width: -webkit-fill-available;
+    padding: 10px;
+    font-size: 1.5rem;
+  }
+</style>
+
+<form>
+  <input type="text" placeholder="What needs to be done?">
+</form>
+<ul></ul>
+`;
+
+class TodoApp extends HTMLElement {
+  constructor() {
+    super();
+    this._shadowRoot = this.attachShadow({ mode: 'open' });
+    this._shadowRoot.appendChild(template.content.cloneNode(true));
+
+    this.$todoList = this._shadowRoot.querySelector('ul');
+    this.$input = this._shadowRoot.querySelector('input');
+
+    this.$form = this._shadowRoot.querySelector('form');
+    this.$form.addEventListener('submit', this._addTodo.bind(this))
+
+    this._todos = [];
+  }
+
+  _renderTodoList() {
+    this.$todoList.innerHTML = '';
+
+    this._todos.forEach((todo, index) => {
+      let $todoItem = document.createElement('li');
+      $todoItem.innerHTML = todo.text;
+      this.$todoList.appendChild($todoItem);
+    })
+  }
+
+  get todos() {
+    return this._todos;
+  }
+
+  _addTodo(event) {
+    event.preventDefault();
+
+    const value = this.$input.value;
+    if (value === '') return;
+    const todo = {
+      id: Date.now(),
+      text: value,
+      done: false
+    };
+    this._todos.push(todo);
+    this._renderTodoList();
+    this.$input.value = '';
+  }
+}
+
+window.customElements.define('todo-app', TodoApp);
